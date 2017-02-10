7d604a0d154b6dfa8e2133bebb156eb3fa338d93
iconet <qiuyming@foxmail.com>
2017-02-10 17:32:30 +0800

    Add files via upload

---
+++ s.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>计算结果</title>
+    	<link href="//cdn.bootcss.com/materialize/0.98.0/css/materialize.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://dvite.net/css/amazeui.css" />;
+  </head>
+  <body>
+  <?php $x=$_GET["bjs"]+$_GET["js"]; ?>
+  <center>;
+    <div class="am-panel card-panel"><center class="card-panel"><h1><?php echo "计算完成，和是$x"; ?></h1>
+      <br><a href="index.html" class="waves-effect waves-light btn" style="background-color:skyblue;">返回</a></center></div>
+  </body>
+    </html>
